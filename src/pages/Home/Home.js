81339eea5bd4beef3d43bd5a2cbf72035563e706
Ramon <ramon1234rxm@gmail.com>
2021-10-11 22:20:35 -0300

    feat/style: creating home page and stylize

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import { Button } from '../../components';
+import * as S from './HomeStyled';
+
+export const Home = () => {
+  return (
+  <S.ContainerHome>
+    <Button>Clique!</Button>
+  </S.ContainerHome>
+  );
+};
---
+++ src/pages/Home/HomeStyled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContainerHome = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+`;
