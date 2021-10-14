85eec20676c260ac61f01c0e6b38d6a660eee820
Ramon <ramon1234rxm@gmail.com>
2021-10-14 10:20:19 -0300

    feat: register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Avatar } from '../../components';
+import { useAuth } from '../../contexts';
 import { RegisterForm } from './forms';
 import * as S from './RegisterStyled';
 
 export const Register = () => {
+  const { push } = useHistory();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user.name) {
+      push('/home');
+    }
+  }, [user]);
+
   return (
     <S.ContainerRegister>
         <Avatar/>
--- src/pages/Register/forms/RegisterForm.js
+++ src/pages/Register/forms/RegisterForm.js
@@ -1,7 +1,10 @@
 import { Button, Form, Input } from '../../../components';
+import { useAuth } from '../../../contexts';
 import { validationRegister } from '../validations';
 
 export const RegisterForm = () => {
+  const { registerUser, authLoading } = useAuth();
+
   const initValues = {
     name: '',
     email1: '',
@@ -9,20 +12,23 @@ export const RegisterForm = () => {
     password1: '',
     password2: '',
   };
+
+  const handleSubmit = async (values) => {
+    await registerUser(values);
+  };
+
   return (
     <Form
       initialValues={initValues}
       validationSchema={validationRegister}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
+      onSubmit={handleSubmit}
       >
-      <Input type='text' name='name' label='Nome completo' placeholder='Nome completo' />
-      <Input type='email' name='email1' label='Informe seu email' placeholder='estartando@devs.com' />
-      <Input type='email' name='email2' label='Confirme seu email' placeholder='estartando@devs.com' />
-      <Input type='password' name='password1' label='Informe sua senha' placeholder='*******' />
-      <Input type='password' name='password2' label='Informe sua senha' placeholder='*******' />
-      <Button type='submit' >
+      <Input type='text' name='name' label='Nick do Github' placeholder='ramonxm' disabled={authLoading}/>
+      <Input type='email' name='email1' label='Informe seu email' placeholder='estartando@devs.com' disabled={authLoading}/>
+      <Input type='email' name='email2' label='Confirme seu email' placeholder='estartando@devs.com' disabled={authLoading}/>
+      <Input type='password' name='password1' label='Informe sua senha' placeholder='*******' disabled={authLoading}/>
+      <Input type='password' name='password2' label='Informe sua senha' placeholder='*******' disabled={authLoading}/>
+      <Button type='submit' isLoading={authLoading} disabled={authLoading}>
         Cadastrar
       </Button>
   </Form>
