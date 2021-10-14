1b110247736cf0ab82987a386cceb930dc302d76
Ramon <ramon1234rxm@gmail.com>
2021-10-14 10:15:27 -0300

    chore: update infra, api and routes

--- src/infra/api/base.js
+++ src/infra/api/base.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '',
+  baseURL: 'https://api.github.com/users',
+  headers: { 'Content-Type': 'application/json' },
 });
--- src/infra/routes/Routes.js
+++ src/infra/routes/Routes.js
@@ -1,11 +1,27 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useAuth } from '../../contexts';
 import { Home, Register } from '../../pages';
+
+const CustomRoute = ({ isPrivate, ...restRoutes }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (isPrivate && !isAuthenticated) {
+    return <Redirect to='/' />;
+  }
+
+  return <Route {...restRoutes} />;
+};
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/register" component={Register}/>
+      <CustomRoute exact path="/" component={Register}/>
+      <CustomRoute isPrivate exact path="/home" component={Home}/>
     </Switch>
   );
+};
+
+CustomRoute.propTypes = {
+  isPrivate: PropTypes.bool,
 };
