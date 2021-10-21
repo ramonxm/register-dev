9e9d7555f89d89c0fdf6e9d8aa89320d26a49d38
Ramon <ramon1234rxm@gmail.com>
2021-10-21 10:41:39 -0300

    feat: make a routes for backend

---
+++ backend/src/router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import authMiddleware from "./app/middlewares/authMiddleware";
+
+import UserController from "./app/controllers/UserController";
+import AuthController from "./app/controllers/AuthController";
+
+const routes = Router();
+
+routes.post('/user', UserController.store);
+routes.post('/auth', AuthController.authenticated);
+routes.use(authMiddleware);
+routes.get('/user/:id', UserController.getById);
+
+export default routes;
